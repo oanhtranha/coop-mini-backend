deb1b7d629fa59304319bd53eba6eb15fb61718c
oanhtranha <oanhtranha@gmail.com>
2025-11-12 23:17:22 +0100

    Fix

--- src/app.js
+++ src/app.js
@@ -5,7 +5,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
-const path_1 = __importDefault(require("path"));
 const admin_1 = __importDefault(require("./routes/admin"));
 const user_1 = __importDefault(require("./routes/user"));
 const app = (0, express_1.default)();
@@ -14,7 +13,7 @@ app.use(express_1.default.json());
 // ===========================
 // Routes
 // ===========================
-app.use("/uploads", express_1.default.static(path_1.default.join(__dirname, "../uploads")));
+// app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 app.use("/admin", admin_1.default); // /admin/products
 app.use("/user", user_1.default); // /signup, /login
 exports.default = app;
