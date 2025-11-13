7b5287616b8fb2955722f8961d72e9cdb4d0e9bd
oanhtranha <oanhtranha@gmail.com>
2025-11-13 14:50:06 +0100

    no message

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,18 @@ import userRoutes from "./routes/user";
 const app = express();
 
 
+app.use(cors());
+// app.use(
+//   cors({
+//     origin: [
+//       "http://localhost:3000", // dev frontend
+//       "https://coop-admin.vercel.app", // frontend production URL
+//     ],
+//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//     credentials: true,
+//   })
+// );
 app.use(express.json());
 
 app.use("/admin", adminRoutes); // /admin/products
